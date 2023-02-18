ad6ee0896d59bfb87961afaf5ec60786d413c184
maddjenn <90150746+maddjenn@users.noreply.github.com>
2023-02-17 19:48:47 -0500

    Update proj1.py

--- proj1.py
+++ proj1.py
@@ -208,17 +208,14 @@ def nat_pct(data, col_list):
         dictionary of the national demographic percentages
 
     '''
-    data_totals = {}    
+    data_totals = {}
     for col in col_list:
-            for region in data:
-                value = data[region].get(col, 0)
-                data_totals[col] = data_totals.get(col, 0) + value
+        col_total = sum(data[region].get(col, 0) for region in data)
+        data_totals[col] = col_total
     total_population = sum(data_totals.values())
     for col, total in data_totals.items():
         percentage = (total / total_population) * 100
         data_totals[col] = percentage
-    
-    return data_totals
 
 def nat_diff(sat_data, census_data):
     '''
